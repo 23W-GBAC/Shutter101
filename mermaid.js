6afd476bf1a281610a8df1407d70b70ce17c84ae
Jayson Dabu <148863168+jysndabuio@users.noreply.github.com>
2023-12-25 01:35:19 +0100

    Update mermaid.js

--- mermaid.js
+++ mermaid.js
@@ -1,1 +1,12 @@
-<script src="/Shutter101/mermaid.js"></script>
+<script>
+    const script = document.createElement('script');
+    script.src = './mermaid.js';
+    document.head.appendChild(script);
+
+    script.onload = () => {
+        mermaid.initialize({ startOnLoad: true });
+
+        // Your Mermaid chart code goes here
+        mermaid.init(undefined, document.querySelectorAll('.mermaid'));
+    };
+</script>
