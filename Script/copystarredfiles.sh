f1ce4783d555b097ceb086c3d596b716ede6451c
Jayson Dabu <148863168+jysndabuio@users.noreply.github.com>
2024-01-01 21:09:48 +0100

    Update copystarredfiles.sh

--- Script/copystarredfiles.sh
+++ Script/copystarredfiles.sh
@@ -2,7 +2,7 @@ for file in *.CR3; do
         rating=$(exiftool -Rating "$file" | awk -F': ' '{print $2}')
         echo "File: $file, Rating: $rating"
 
-        if [[ "$rating" != "0" ]]; then
+        if [[ "$rating" == "1" ]]; then
             echo "Copying $file to ~/Pictures/StarredImages/"
             cp "$file" ~/Pictures/StarredImages/
         fi
